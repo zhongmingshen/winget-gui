067efb98da2a02894e58f1e2285e3d94ffb18ebe
“zhongmingshen” <747909405@qq.com>
2025-12-02 00:12:35 +0800

    保存本地更改：为与远端整合创建本地提交

---
+++ src/renderer/main.js
@@ -0,0 +1,53 @@
+"use strict";
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    var desc = Object.getOwnPropertyDescriptor(m, k);
+    if (!desc || ("get" in desc ? !m.__esModule : desc.writable || desc.configurable)) {
+      desc = { enumerable: true, get: function() { return m[k]; } };
+    }
+    Object.defineProperty(o, k2, desc);
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __setModuleDefault = (this && this.__setModuleDefault) || (Object.create ? (function(o, v) {
+    Object.defineProperty(o, "default", { enumerable: true, value: v });
+}) : function(o, v) {
+    o["default"] = v;
+});
+var __importStar = (this && this.__importStar) || (function () {
+    var ownKeys = function(o) {
+        ownKeys = Object.getOwnPropertyNames || function (o) {
+            var ar = [];
+            for (var k in o) if (Object.prototype.hasOwnProperty.call(o, k)) ar[ar.length] = k;
+            return ar;
+        };
+        return ownKeys(o);
+    };
+    return function (mod) {
+        if (mod && mod.__esModule) return mod;
+        var result = {};
+        if (mod != null) for (var k = ownKeys(mod), i = 0; i < k.length; i++) if (k[i] !== "default") __createBinding(result, mod, k[i]);
+        __setModuleDefault(result, mod);
+        return result;
+    };
+})();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const vue_1 = require("vue");
+const App_vue_1 = __importDefault(require("./App.vue"));
+require("ant-design-vue/dist/antd.css");
+require("./styles.scss");
+const ant_design_vue_1 = __importDefault(require("ant-design-vue"));
+const Icons = __importStar(require("@ant-design/icons-vue"));
+const app = (0, vue_1.createApp)(App_vue_1.default);
+app.use(ant_design_vue_1.default);
+// register icons globally
+Object.entries(Icons).forEach(([k, v]) => {
+    // @ts-ignore
+    app.component(k, v);
+});
+app.mount("#app");
+//# sourceMappingURL=main.js.map
