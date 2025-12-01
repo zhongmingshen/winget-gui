41a9654912f579a6bce03cf61ef433b9c7c53480
沈忠明 <747909405@qq.com>
2025-12-01 23:59:07 +0800

    上传 PackageDetails.vue 完整内容

--- src/renderer/components/PackageDetails.vue
+++ src/renderer/components/PackageDetails.vue
@@ -1,1 +1,31 @@
-(file content omitted due to length; full file will be pushed)
+<template>
+    <div>
+        <h3>{{ t('packageDetails.title') }}</h3>
+        <div v-if="pkg">
+            <p><strong>{{ t('packageDetails.name') }}:</strong> {{ pkg.name }}</p>
+            <p><strong>{{ t('packageDetails.id') }}:</strong> {{ pkg.id }}</p>
+            <p><strong>{{ t('packageDetails.version') }}:</strong> {{ pkg.version }}</p>
+        </div>
+        <div v-else>{{ t('packageDetails.empty') }}</div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, PropType } from 'vue'
+import { useI18n } from 'vue-i18n'
+
+export default defineComponent({
+    name: 'PackageDetails',
+    props: { pkg: { type: Object as PropType<any>, required: false } },
+    setup() {
+        const { t } = useI18n()
+        return { t }
+    }
+})
+</script>
+
+<style scoped>
+p {
+    margin: 6px 0
+}
+</style>
